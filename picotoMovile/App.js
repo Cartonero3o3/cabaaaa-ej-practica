bf1893e9179e4e53b46a24d22eac6e8569d75712
Caba <kirito98.1234@gmail.com>
2025-09-17 16:54:57 -0300

    Safe area return error

--- picotoMovile/App.js
+++ picotoMovile/App.js
@@ -1,4 +1,18 @@
-import { StyleSheet, StatusBar, Image, Button, Text, ScrollView, View } from 'react-native';
+import {
+  StyleSheet,
+  NavBar,
+  StatusBar,
+  Image,
+  Button,
+  Text,
+  ScrollView,
+  View,
+} from "react-native";
+
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import React, { useState } from "react";
 
 export default function App() {
@@ -6,7 +20,7 @@ export default function App() {
   const insets = useSafeAreaInsets();
   const increment = () => {
     if (count == 0) {
-      setCount(1);  
+      setCount(1);
     } else {
       setCount(0);
     }
@@ -14,37 +28,42 @@ export default function App() {
 
   const juego = {
     nombre: "Carlos",
-    image: "https://imgs.search.brave.com/UOSfU62692jX6WbhSjcF6jRY3BI-wJCphYOmjnLvT0g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ4/MzEyNTY2MS9mci9w/aG90by9sb25kb24t/ZW5nbGFuZC1zcGFp/bnMtZm9ybWVyLWtp/bmctanVhbi1jYXJs/b3MtaS13YXRjaGVz/LWR1cmluZy10aGUt/dWVmYS1jaGFtcGlv/bnMtbGVhZ3VlLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1W/ZllJendjaW9IQXMz/LW5lcWxOc2VMS19m/UGprQ2lBdmVxc0l3/dm5uR2lJPQ",
+    image:
+      "https://imgs.search.brave.com/UOSfU62692jX6WbhSjcF6jRY3BI-wJCphYOmjnLvT0g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ4/MzEyNTY2MS9mci9w/aG90by9sb25kb24t/ZW5nbGFuZC1zcGFp/bnMtZm9ybWVyLWtp/bmctanVhbi1jYXJs/b3MtaS13YXRjaGVz/LWR1cmluZy10aGUt/dWVmYS1jaGFtcGlv/bnMtbGVhZ3VlLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1W/ZllJendjaW9IQXMz/LW5lcWxOc2VMS19m/UGprQ2lBdmVxc0l3/dm5uR2lJPQ",
     botonTitle: "presionameeeee",
     score: 50,
-    descripcion: "Holaa soy Juan Carloss y te vigilo desde lejosss"
+    descripcion: "Holaa soy Juan Carloss y te vigilo desde lejosss",
   };
 
   return (
-    <SafeAreaProvider style={styles.safeArray}> 
-    <ScrollView style={styles.scroll}>
-      {Array.from({ length: 30 }).map((_, i) => (
-        <View key={i} style={styles.card}>
-          <StatusBar 
-            barStyle="light-content"   
-            backgroundColor="#6200EE" 
-            translucent={false}        
-          />
-          <Image 
-            source={{ uri: juego.image }}
-            style={styles.image}
-          />
-          <Text style={styles.title}> {juego.nombre} + Score {juego.score} </Text>
-          <Text style={styles.description}>{juego.descripcion}</Text>
-          
-          <Button 
-            title={juego.botonTitle} 
-            onPress={() => alert("walaaaa")} 
-            color="orange" 
-          />
-        </View>
-      ))}
-    </ScrollView>
+    <SafeAreaProvider>
+      <ScrollView
+        style={[
+          styles.scroll,
+          {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+          },
+        ]}
+      >
+        {Array.from({ length: 30 }).map((_, i) => (
+          <View key={i} style={styles.card}>
+            <Image source={{ uri: juego.image }} style={styles.image} />
+            <Text style={styles.title}>
+              {" "}
+              {juego.nombre} + Score {juego.score}{" "}
+            </Text>
+            <Text style={styles.description}>{juego.descripcion}</Text>
+            <Button
+              title={juego.botonTitle}
+              onPress={() => alert("walaaaa")}
+              color="orange"
+            />
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaProvider>
   );
 }
@@ -54,13 +73,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  safeArray: {
-    flex: 1, 
-    paddingTop: insets.top,
-    paddingBottom: insets.bottom,
-    paddingLeft: insets.left,
-    paddingRight: insets.right,
-  },
   card: {
     alignSelf: "center",
     backgroundColor: "#fff",
@@ -69,11 +81,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f9f9f9",
     borderRadius: 10,
     alignItems: "center",
-    elevation: 3, 
-    shadowColor: "#000", 
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    shadowOffset: { width: 0, height: 2 },
+    elevation: 3,
   },
   image: {
     width: 200,
